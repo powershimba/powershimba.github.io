291047c89239a1b38d3cea1bf47dfe668fde3e00
powershimba <99706343+powershimba@users.noreply.github.com>
2023-05-16 13:31:38 -0700

    daily commit

---
+++ supergulp/gulpfile.babel.js
@@ -0,0 +1,93 @@
+import gulp from "gulp";
+import gpug from "gulp-pug";
+import del from "del";
+import ws from "gulp-webserver";
+import image from "gulp-image";
+import autoprefixer from "gulp-autoprefixer";
+import miniCSS from "gulp-mini-css";
+import bro from "gulp-bro";
+import babelify from "babelify";
+import ghPages from "gulp-gh-pages";
+
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
+
+const routes = {
+    pug: {
+        watch: "src/**/*.pug",
+        src: "src/*.pug",
+        dest: "build"
+    },
+    img: {
+        src: "src/img/*",
+        dest: "build/img"
+    },
+    scss: {
+        watch: "src/scss/*.scss",
+        src:"src/scss/style.scss",
+        dest: "build/css"
+    },
+    js : {
+        watch: "src/js/**/*.js",
+        src: "src/js/main.js",
+        dest: "build/js"
+    }
+};
+
+const pug = () => 
+    gulp
+        .src(routes.pug.src)
+        .pipe(gpug())
+        .pipe(gulp.dest(routes.pug.dest));
+
+
+const webserver = () => gulp.src("build").pipe(ws({livereload: true, open:true}));
+
+const clean = () => del(["build/", ".publish"]);
+
+const js = () => gulp
+    .src(routes.js.src)
+    .pipe(bro({
+        transform: [
+            babelify.configure({
+                presets:["@babel/preset-env"]
+            }),
+            ["uglifyify", {global:true}]
+        ]
+    }))
+    .pipe(gulp.dest(routes.js.dest));
+
+const watch = () => {
+    gulp.watch(routes.pug.watch, pug);
+    gulp.watch(routes.scss.watch, styles);
+    gulp.watch(routes.js.watch, js);
+}
+
+const img = () => 
+    gulp
+        .src(routes.img.src)
+        .pipe(image())
+        .pipe(gulp.dest(routes.img.dest));
+
+const styles = () =>
+    gulp
+        .src(routes.scss.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(miniCSS())
+        .pipe(gulp.dest(routes.scss.dest));
+
+const ghDeploy = () => gulp
+    .src("build/**/*")
+    .pipe(ghPages());
+
+const prepare = gulp.series([clean, img]); 
+
+const assets = gulp.series([pug, styles, js]);
+
+const postDev = gulp.parallel([webserver, watch]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
+export const build = gulp.series([prepare, assets]);
+export const deploy = gulp.series([build, ghDeploy]);
